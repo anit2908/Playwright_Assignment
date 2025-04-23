d0290994f4a796d192da23f639ea5b40864b6583
anit2908 <maharanaanit@gmail.com>
2025-04-23 09:47:15 +0530

    Update Assign1_Primitive.js

--- Week1_Assignments/Assign1_Primitive.js
+++ Week1_Assignments/Assign1_Primitive.js
@@ -1,16 +1,18 @@
+//Assignment of variables
 let firstName = "Anit"
 let companyName = null
 let mobileNumber = 808888888n
 let isAutomation = true
 let hasPlaywright 
 
+//printing the variables
 console.log(firstName)
 console.log(companyName)
 console.log(mobileNumber)
 console.log(isAutomation)
 console.log(hasPlaywright)
 
-
+//printing the type of Variables
 console.log(typeof(firstName))
 console.log(typeof(companyName))
 console.log(typeof(mobileNumber))
